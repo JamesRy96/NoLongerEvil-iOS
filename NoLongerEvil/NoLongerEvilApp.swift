42ac78a97304956d871fc4c4c344500fc24008a4
JamesRy96 <33638980+JamesRy96@users.noreply.github.com>
2026-02-20 00:23:19 -0500

    Initial Commit

---
+++ NoLongerEvil/NoLongerEvilApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoLongerEvilApp.swift
+//  NoLongerEvil
+//
+//  Created by Trevor Pease on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct NoLongerEvilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoLongerEvilTests/NoLongerEvilTests.swift
@@ -0,0 +1,16 @@
+//
+//  NoLongerEvilTests.swift
+//  NoLongerEvilTests
+//
+//  Created by Trevor Pease on 2/20/26.
+//
+
+import Testing
+
+struct NoLongerEvilTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
